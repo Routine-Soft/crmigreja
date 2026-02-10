36a9dd978cbaf6bc77a7b7ead976ab740848d107
Stanley Andrade <stanleyandrade95@gmail.com>
2026-02-10 15:56:43 -0300

    register update

--- app/register/user/page.tsx
+++ app/register/user/page.tsx
@@ -81,7 +81,7 @@ export default function UserRegisterPage() {
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
                             </svg>
                         </div>
-                        <h1 className="text-3xl font-bold text-gray-800">Criar Conta de membro</h1>
+                        <h1 className="text-3xl font-bold text-gray-800">Criar Conta</h1>
                         <p className="text-gray-600">Cadastre-se em poucos minutos</p>
                     </div>
 
